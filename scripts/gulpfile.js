44a1904e57154a5915792c962747eb89b73cf9e4
by77er <by77er@gmail.com>
2023-12-24 09:38:02 -0500

    Add build and push scripts

---
+++ scripts/gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var screeps = require('gulp-screeps');
+var credentials = require('../credentials.js');
+// See https://github.com/screepers/gulp-screeps
+
+gulp.task('screeps', function() {
+  return gulp.src('../build/*.js')
+    .pipe(screeps(credentials));
+});
