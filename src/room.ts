31599f8318754b0c8b6ced919c12246c64f2d673
by77er <by77er@gmail.com>
2023-12-25 09:03:57 -0500

    Readd auto-defense

--- src/room.ts
+++ src/room.ts
@@ -1,3 +1,4 @@
+import { defend } from './defend';
 import { HarvestCreep, harvest } from './harvest';
 
 export function runRoomStrategy(room: Room) {
@@ -23,13 +24,21 @@ export function runRoomStrategy(room: Room) {
 
 function rc1(room: Room) {
     const starterCreeps = room.find(FIND_MY_CREEPS, {filter: c => c.memory['role'] === 'starter'});
+    const defenderCreeps = room.find(FIND_MY_CREEPS, {filter: c => c.memory['role'] === 'defender'});
+    const hostiles = room.find(FIND_HOSTILE_CREEPS);
     const spawn = room.find(FIND_MY_SPAWNS)[0];
 
-    if (starterCreeps.length < 4) {
+    if (hostiles.length > 0 && defenderCreeps.length < 2) {
+        spawn.spawnCreep([ATTACK, MOVE], `defender${Game.time}`, {memory: {role: 'defender'}});
+    } else if (starterCreeps.length < 4) {
         spawn.spawnCreep([WORK, CARRY, MOVE], `starter${Game.time}`, {memory: {role: 'starter'}});
     }
 
     for (const creep of starterCreeps) {
         harvest(creep as HarvestCreep);
+    }
+
+    for (const creep of defenderCreeps) {
+        defend(creep);
     }
 }
